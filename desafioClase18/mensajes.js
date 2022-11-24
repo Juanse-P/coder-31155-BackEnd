698857e217ccd10d59a65794b4105a60e12f2b6f
Juanse-P <jspp020205@gmail.com>
2022-11-23 19:10:29 -0500

    desafio clase 18

---
+++ desafioClase18/mensajes.js
@@ -0,0 +1,52 @@
+const mensajes = [
+    {
+        email: 'emailNo1@gmail.com',
+        message: 'mensajeNo1',
+        date: 'fechaNo1'
+    },
+    {
+        email: 'emailNo2@gmail.com',
+        message: 'mensajeNo2',
+        date: 'fechaNo2'
+    },
+    {
+        email: 'emailNo3@gmail.com',
+        message: 'mensajeNo3',
+        date: 'fechaNo3'
+    },
+    {
+        email: 'emailNo4@gmail.com',
+        message: 'mensajeNo4',
+        date: 'fechaNo4'
+    },
+    {
+        email: 'emailNo5@gmail.com',
+        message: 'mensajeNo5',
+        date: 'fechaNo5'
+    },
+    {
+        email: 'emailNo6@gmail.com',
+        message: 'mensajeNo6',
+        date: 'fechaNo6'
+    },
+    {
+        email: 'emailNo7@gmail.com',
+        message: 'mensajeNo7',
+        date: 'fechaNo7'
+    },
+    {
+        email: 'emailNo8@gmail.com',
+        message: 'mensajeNo8',
+        date: 'fechaNo8'
+    },
+    {
+        email: 'emailNo9@gmail.com',
+        message: 'mensajeNo9',
+        date: 'fechaNo9'
+    },
+    {
+        email: 'emailNo10@gmail.com',
+        message: 'mensajeNo10',
+        date: 'fechaNo10'
+    }
+]
